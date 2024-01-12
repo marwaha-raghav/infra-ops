b62675895b2bbc77ca7162d36c7744af48d5785e
marwaha-raghav <marwaha.raghav@rediffmail.com>
2024-01-12 13:33:05 -0500

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # infra-ops
+
+## Code-Overview
+This repository contains; code written in python3 that tries to achieve 4 tasks:
+
+- Compress and Encrypt a file
+- Create a backup copy of a file
+- Upload the file to an S3 bucket
+- Compare the MD5 hash
+
+
+### Libraries Used
+
+- boto3 - for aws uploads.
+- datetime - for calculating timestamps
+- hashlib - for egenrating md5 digests of file
+- os -for running os levels commands (unix specific)
+- cryptogrpahy (fernet) for encryption of files.
+- tarfile - for generating tar.gz
+
+### Documenting the Code. 
+The general workflow of the program is as follows
+
+- get_one_hour_prior: This function uses the datetime library to calculate the current time in a specific manner and then calculate the delta of it by 1.
+- we then use this value to generate the file name in the following format syslog.20240112.12 (which has hour-1 value aftet he period)
+- using the value we try to match the output returned while looping through the result returned from os.listdir(), which returns all the files specified the log_file path (/var/log/events)
+- if a match is found, we proceed with the compression of this file using tarfile library and get a tar.gz file, if not found and exception will be dsiplayed.
+- once the tar.gz has been created, we use the fernet library and create a private key to encrypt the file with. 
+- the file is read and an encrypted file is written with the same name appended with .encrypted. 
+- This file is then also copied to another location /var/log/archive using the os.system(cp filename dest_path), the result is also checked for success or failure and an appropriate message is displayed. 
+- An extra step has been added to ask the user for s3 uploads. 
+- the file is then uploaded to the s3 bucket by using the boto3 library and the funtion upload_file() and the reponse is captured,and out of this json response, the e3 tags are extracted (md5 for uploaded file)
+- md5 hash of the local file is also calculated and then compared with the remote uploaded one, if they match then the upload was successful if not then unsuccessful. 
---
+++ compress.py
@@ -0,0 +1,224 @@
+import os
+import datetime
+from datetime import datetime, timedelta
+import tarfile
+from cryptography.fernet import Fernet
+import boto3
+from botocore.exceptions import NoCredentialsError
+import hashlib
+
+# path for creating the backups as described in task 3-b
+backup_path = "var/log/archives"
+
+
+# get current time
+def get_one_hour_prior():
+    """Gets Hours(dd) -1 hour from current Hour.
+
+    This function uses datetime.now and generates current hour minus 1 in 12h format
+    Args:
+        None
+
+    Returns:
+        int current_time: (dd) current hour - 1
+    """
+    raw_current_time = datetime.now()
+    one_hour_prior = raw_current_time - timedelta(hours=1)
+    current_time = one_hour_prior.strftime("%Y%m%d.%H")
+    # print(current_time)
+    return current_time
+
+
+# define the path for the log files
+def logs_directory():
+    """Returns the path for the log files
+
+    This function returns the log files path.
+        None
+
+    Returns:
+        str path_logs
+    """
+    path_logs = "/var/log/events"
+    return path_logs
+
+
+def search_log_directory(hour, file_path):
+    # ls_output = os.system(f'ls {file_path}')
+    file_of_interest = f"syslog.{hour}".strip()
+
+    for file in os.listdir(file_path):
+        if file_of_interest in file:
+            print(f"Success: {file_of_interest} found in the {file_path}")
+
+        else:
+            print("Searching...")
+
+
+# perform the compression of the log files in the file_path
+def compress_logs(hour, file_path):
+    """Creates the tar.gz compressed version of the specified logfile
+
+    This function creates the tar.gz file of the specified log file.
+    args:
+       a. hour(int): The previous hour value
+       b. file_path (str): The path of the log files.
+
+    Returns:
+        None
+    """
+    search_log_directory(hour, file_path)
+    file_of_interest = f"syslog.{hour}".strip()
+    with tarfile.open(f"{file_path}/{file_of_interest}.tar.gz", "w:gz") as tarball:
+        tarball.add(f"{file_path}/{file_of_interest}")
+        tarball_name = f"{file_path}/{file_of_interest}.tar.gz"
+        print(f"Successfully Created {tarball_name}")
+
+
+def generate_encryption_key():
+    """Generates encryption key using Fernet Class
+
+    This function creates encryption key
+    args:
+       None
+
+    Returns:
+        None;
+    """
+    key = Fernet.generate_key()
+    with open("encryption.key", "wb") as enc_key:
+        enc_key.write(key)
+
+
+def encrypt_tarball(file_path, hour):
+    """Encrypts the tarball
+
+    This function encrypts the tarball
+    args:
+       a. hour(int): The previous hour value
+       b. file_path (str): The path of the log files.
+
+    Returns:
+        None
+    """
+    generate_encryption_key()
+    file_of_interest = f"syslog.{hour}".strip()
+    with open("encryption.key", "rb") as key_file:
+        key = key_file.read()
+        # generate instance of Fernet class with the key
+        fernet = Fernet(key)
+        # encrypting the file
+        tarball_name = f"{file_path}/{file_of_interest}.tar.gz"
+        with open(f"{tarball_name}", "rb") as file:
+            tar_contents = file.read()
+        # Encrypt the file data
+        encrypted_tar_contents = fernet.encrypt(tar_contents)
+        with open(f"{tarball_name}.encrypted", "wb") as encrypted_file:
+            encrypted_file.write(encrypted_tar_contents)
+    print("Encryption performed. Key also saved'.")
+
+
+def generate_log_name(hour):
+    logname = f"syslog.{hour}".strip()
+    return logname
+
+
+def generate_tarball_name(file_path, file_of_interest):
+    tarball_name = f"{file_path}/{file_of_interest}.tar.gz.encrypted"
+    return tarball_name
+
+
+def create_local_backup(back_path, file_path, file_of_interest):
+    """Creates a Local Backup
+
+    This copies the tarball to another folder
+    args:
+       a. hour(int): The previous hour value
+       b. file_path (str): The path of the log files.
+       c. back_path (str): New Path for backup
+
+    Returns:
+        None
+    """
+    result = os.system(
+        f"cp {generate_tarball_name(file_path, file_of_interest)} {back_path}"
+    )
+    if result == 0:
+        print("encrypted tar copied successfully \n")
+    else:
+        print("Not copied")
+
+
+# uploading tarball on aws
+def upload_to_s3(tar_ball, bucket, s3_file):
+    """CUploads the file to s3 bucket
+
+    This uploads the file to an s3 bucket
+    args:
+       a. tarball(str): local file to upload
+       b. bucket (str): bucket name
+       c. s3_file: filename/trags in s3 bucket
+
+    Returns:
+        response
+    """
+    s3_bucket = boto3.client("s3")
+
+    try:
+        response = s3_bucket.upload_file(f"{tar_ball}", bucket, s3_file)
+        print("Uploaded")
+        return response
+    except FileNotFoundError:
+        print("tarball not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not provided in any manner")
+        return False
+
+
+def local_md5_check(tarball):
+    # Calculate the Hash of the local file
+
+    # by catching the response in upload_to_s3, capturing the etag from the response
+    # if the etag and the local md5 digest match the upload was successful
+    # since that means that the file integrity was mantained.
+    hash_md5 = hashlib.md5()
+    with open(tarball, "rb") as file:
+        for chunk in iter(lambda: file.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+if __name__ == "__main__":
+    bucket_name = "kyk-infraops-test"
+    current_date = datetime.now()
+    # Task 3-a Compress
+    compress_logs(get_one_hour_prior(), logs_directory())
+    # Tasl 3-a Encrypt
+    encrypt_tarball(logs_directory(), get_one_hour_prior())
+    # Task 3-b Create a Backup
+    create_local_backup(
+        backup_path, logs_directory(), generate_log_name(get_one_hour_prior())
+    )
+    reply = input(
+        "----  Do you want to proceed with the s3 upload? Make sure creds are steup [Y/N]-------"
+    )
+    if reply.lower() == "y":
+        s3_file_suffix = generate_log_name(get_one_hour_prior())
+        s3_file_name = f"type=events/year={current_date.year}/month={current_date.month:02d}/day={current_date.day:02d}/{s3_file_suffix}"
+        local_tarball_name = generate_tarball_name(
+            logs_directory(), generate_log_name(get_one_hour_prior)
+        )
+        # Task 3-c Upload to S3
+        s3_upload_response = upload_to_s3(local_tarball_name, bucket_name, s3_file_name)
+        local_md5_digest = local_md5_check(local_tarball_name)
+        # Task 3-d md5 Check
+        if s3_upload_response is not None:
+            etags = s3_upload_response["ETag"].strip('"')
+            if etags == local_md5_check:
+                print("Hashes match, upload went through")
+            else:
+                print("hashes do not match")
+    else:
+        print("You Chose not to upload to S3")
+        exit(0)
